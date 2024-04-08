8d25403c08f8bf8ba3bd4ee36b2fb5db97b7c371
BDunnCode <135765583+BDunnCode@users.noreply.github.com>
2024-04-08 00:16:31 -0500

    add details component

---
+++ app/components/Details.js
@@ -0,0 +1,41 @@
+import Image from "next/image";
+
+const Details = () => {
+  return (
+    <div className="max-w-6xl mx-auto mt-20">
+      <div className="grid grid-cols-1 lg:grid-cols-2">
+        
+        <div classname="relative h-96 aspect-ratio-1 overflow-hidden shadow-md">
+          <Image 
+            src="/facelessmen.webp"
+            alt="art"
+            height={150}
+            width={300}
+          />
+        </div>
+
+        <div className="flex flex-col justify-between">
+          <h1 className="text-3xl font-semibold text-[#5B20B6]">Painting Name</h1>
+          <p className="text-gray-500 text-lg mt-4">Painting Description</p>
+          
+          <div className="mt-5">
+            <span className="text-2xl font-semibold text-[#5B20B6]">$100</span>
+          </div>
+
+          <div className="mt-6 flex flex-col text-gray">
+            <label htmlFor="">Qty</label>
+            <input type="text" defaultValue={1} className="w-20 px-4 h-10 border
+            border-gray-300 rounded-md"/>
+          </div>
+
+          <div className="mt-6">
+            <button className="bg-[#5B20B6] text-white py-3 px-6 rounded-md">Add to Cart</button>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  )
+}
+
+export default Details
---
+++ app/details/page.js
@@ -0,0 +1,15 @@
+import Details from '../components/Details';
+import Header from '../components/Header';
+
+
+const page = () => {
+  return (
+    <div>
+      <Header />
+      <Details />
+
+    </div>
+  )
+}
+
+export default page
