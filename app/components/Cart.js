028a1781c3e5e441fc912db71df6a681fb1cdf1a
BDunnCode <135765583+BDunnCode@users.noreply.github.com>
2024-04-09 12:46:21 -0500

    update cart cleanup, add link to header

--- app/components/Cart.js
+++ app/components/Cart.js
@@ -6,6 +6,8 @@ import useCartStore from './../cartStore';
 const Cart = () => {
   const cart = useCartStore(state => state.cart);
   const removeFromCart = useCartStore(state => state.removeFromCart);
+  const totalItems = useCartStore(state => state.totalItems);
+  const cartTotal = useCartStore(state => state.cartTotal);
 
   const handleRemoveFromCart = (productId) => {
     removeFromCart(productId)
@@ -27,14 +29,14 @@ const Cart = () => {
 
         <tbody>
           {
-              cart.map(product => (
-                <tr className="hover:bg-gray-50 text-center border-b text-[#5B20B6] border-gray-300" key={product.id}>
-                  <td className="flex items-center py-2 px-4">
+              cart?.map(product => (
+                <tr key={product?.id} className="hover:bg-gray-50 text-center border-b text-[#5B20B6] border-gray-300" >
+                  <td className="flex items-center py-2 px-4" >
                     <Image 
-                      src="/facelessmen.webp"
+                      src={product?.image}
                       alt="img"
-                      height={30}
-                      width={30}
+                      height={50}
+                      width={50}
                       className="mr-2"
                     />
                     {product.name}
@@ -49,6 +51,17 @@ const Cart = () => {
           }
         </tbody>
       </table>
+
+      <div>
+        <div className="space-x-4 mt-10">
+          <span className="text-md font-semibold text-[#5B20B6]">Total:</span>
+          <span className="text-md font-semibold text-[#5B20B6]">${cartTotal}</span>
+        </div>
+        
+        <div className="mt-6 mx-auto space-y-4 max-w-sm ">
+          <button className="bg-[#5B20B6] text-white py-3 px-6 rounded-md w-full text-lg font-semibold">Checkout</button>
+        </div>
+      </div>
     </div>
   )
 }
--- app/components/Header.js
+++ app/components/Header.js
@@ -14,15 +14,17 @@ function Header() {
   return (
     <div className="p-3 border-b-2 border-[#F5F3FF]">
       <div className="max-w-6xl mx-auto flex justify-between">
-        <div className="flex gap-2 items-center text-2xl lg:text-3xl font-bold">
-          <Image 
-            src="/facelessmen.webp"
-            alt="logo"
-            height={50}
-            width={50}
-          />
-          <h1 className="">Artistry Market</h1>
-        </div>
+        <Link href='/'>
+          <div className="flex gap-2 items-center text-2xl lg:text-3xl font-bold">
+            <Image 
+              src="/facelessmen.webp"
+              alt="logo"
+              height={50}
+              width={50}
+            />
+            <h1 className="">Artistry Market</h1>
+          </div>
+        </Link>
         <div className="flex items-center relative">
           <Link href='/cart'>
             <FaShoppingCart className="text-3xl text-[#5B20B6] cursor-pointer"  />
