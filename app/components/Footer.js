8af209698f1bcbe15f63154a4b19cc1bb741193a
BDunnCode <135765583+BDunnCode@users.noreply.github.com>
2024-04-10 14:36:22 -0500

    updated footer using map / config method

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import Link from "next/link";
+import Image from "next/image";
+
+import { siteConfig } from "../../config/site";
+
 
 const Footer = () => {
   return (
-    <div className="p-10">
-      <h1 className="text-center text-lg">Footer</h1>
-    </div>
-  )
-}
+    <footer className="border-t">
+      <div className="mx-auto px-6 py-6 sm:py-10 sm:p">
+        <nav className="" aria-label="">
+          <ul className="columns-2 sm:flex sm:justify-center sm:space-x-12 mt-2">
+            {siteConfig.footer.map((item) => (
+              <li key={item.name}>
+                <Link href="/" className="text-sm leading-6">
+                  {item.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <Link 
+          href="#"
+          className="mt-10 text-center block text-xs leading-5"
+        >
+          &copy; {new Date().getFullYear()} {siteConfig.name} LLC. All rights reserved.
+        </Link>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
---
+++ config/site.js
@@ -0,0 +1,13 @@
+
+export const siteConfig = {
+  name: "Artistry Market",
+  description: "Art Shop",
+  footer: [
+    { name: "Home", href: "/" },
+    { name: "About", href: "#" },
+    { name: "Terms & Conditions", href: "#" },
+    { name: "Shipping & Return Policy", href: "#" },
+    { name: "Privacy Policy", href: "#" },
+    { name: "FAQ", href: "#" },
+  ]
+}
